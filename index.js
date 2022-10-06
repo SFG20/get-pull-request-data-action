cc16f708b08365fd41e1b3ad7aabd8e877ba7763
Dave Cozens <dave@davecozens.com>
2022-10-06 13:49:06 +0100

    Use vercel/ncc

---
+++ index.js
@@ -0,0 +1,14 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+    // `who-to-greet` input defined in action metadata file
+    const issue_number = core.getInput('issue_number');
+    console.log(`issue_number ${issue_number}!`);
+    core.setOutput("data", {data:1, issue_number});
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+} catch (error) {
+    core.setFailed(error.message);
+}
