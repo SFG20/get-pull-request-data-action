a2fd6705475944c15d5b8a83a50db5470ffbf550
Dave Cozens <dave@davecozens.com>
2022-10-07 09:29:27 +0100

    WIP

--- dist/index.js
+++ dist/index.js
@@ -9695,6 +9695,7 @@ try {
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
     console.log(`The event payload: ${payload}`);
+    console.log('huzzah!')
 } catch (error) {
     core.setFailed(error.message);
 }
--- update.sh
+++ update.sh
@@ -5,6 +5,6 @@ ncc build index.js --license licenses.txt
 git add dist/index.js
 git add dist/licences.txt
 git add *
-git tag -a -f -m "Release test" v1.0
 git commit -m 'WIP'
+git tag -a -f -m "Release test" v1.0
 git push --follow-tags
