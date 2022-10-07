d5e343079c557ee17638f8ed2c69b64dcfa3b521
Dave Cozens <dave@davecozens.com>
2022-10-07 09:42:24 +0100

    WIP

--- dist/index.js
+++ dist/index.js
@@ -9687,17 +9687,33 @@ var __webpack_exports__ = {};
 const core = __nccwpck_require__(9902);
 const github = __nccwpck_require__(1890);
 
-try {
-    // `who-to-greet` input defined in action metadata file
-    const issue_number = core.getInput('issue_number');
-    console.log(`issue_number ${issue_number}!`);
-    core.setOutput("data", {data:12, issue_number});
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
-    console.log('huzzah!')
-} catch (error) {
-    core.setFailed(error.message);
+
+
+async function run(){
+    const myToken = core.getInput('myToken');
+    const octokit = github.getOctokit(myToken)
+
+    try {
+        // `who-to-greet` input defined in action metadata file
+        const issue_number = core.getInput('issue_number');
+        console.log(`issue_number ${issue_number}!`);
+        core.setOutput("data", {data: 12, issue_number});
+        // Get the JSON webhook payload for the event that triggered the workflow
+        const payload = JSON.stringify(github.context.payload, undefined, 2)
+        console.log(`The event payload: ${payload}`);
+
+        const {data: pullRequest} = await octokit.rest.pulls.get({
+            pull_number: issue_number
+        });
+
+        console.log({pullRequest});
+
+        console.log('huzzah!')
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+
+
 }
 
 
@@ -9706,6 +9722,8 @@ try {
 
 
 
+
+run();
 
 })();
 
--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
-    // `who-to-greet` input defined in action metadata file
-    const issue_number = core.getInput('issue_number');
-    console.log(`issue_number ${issue_number}!`);
-    core.setOutput("data", {data:12, issue_number});
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
-    console.log('huzzah!')
-} catch (error) {
-    core.setFailed(error.message);
+
+
+async function run(){
+    const myToken = core.getInput('myToken');
+    const octokit = github.getOctokit(myToken)
+
+    try {
+        // `who-to-greet` input defined in action metadata file
+        const issue_number = core.getInput('issue_number');
+        console.log(`issue_number ${issue_number}!`);
+        core.setOutput("data", {data: 12, issue_number});
+        // Get the JSON webhook payload for the event that triggered the workflow
+        const payload = JSON.stringify(github.context.payload, undefined, 2)
+        console.log(`The event payload: ${payload}`);
+
+        const {data: pullRequest} = await octokit.rest.pulls.get({
+            pull_number: issue_number
+        });
+
+        console.log({pullRequest});
+
+        console.log('huzzah!')
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+
+
 }
 
 
@@ -20,3 +36,5 @@ try {
 
 
 
+
+run();
