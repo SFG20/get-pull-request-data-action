2d606aa4e51d6d5c146473c944e6ba4cb7b92057
Dave Cozens <dave@davecozens.com>
2022-10-07 09:26:39 +0100

    WIP

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ try {
     // `who-to-greet` input defined in action metadata file
     const issue_number = core.getInput('issue_number');
     console.log(`issue_number ${issue_number}!`);
-    core.setOutput("data", {data:1, issue_number});
+    core.setOutput("data", {data:12, issue_number});
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
     console.log(`The event payload: ${payload}`);
